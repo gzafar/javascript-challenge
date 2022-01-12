70c469941a484e19b58ef194082c0c5b8625f63b
unknown <grazielzafar@gmail.com>
2022-01-11 23:51:52 -0600

    referenced table and added values in app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
+console.log("app.js loaded");
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+
+tableData.forEach(function (ufoSightings) {
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSightings).forEach(function ([key, value]) {
+        console.log(`key = ${key}, value = ${value}`);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+
+});
